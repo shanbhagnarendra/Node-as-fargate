ee16d0bfca54185a8b286ee3a76a69de9c6359ba
Narendra Shanbhag <nshanbhag@conversantmedia.com>
2021-12-09 10:06:54 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 //app.use(express.static(publicDir));
 
-app.get("/", (req, res) => res.send("NodeJS on ECS Fargate with CodeDEploy and Jenkins CodeDeployDefault.ECSLinear10PercentEvery1Minutes!"));
+app.get("/", (req, res) => res.send("NodeJS on ECS Fargate Demo with Blue/Green"));
 
 app.get("/health", (req, res) => {
     res.status(200);
